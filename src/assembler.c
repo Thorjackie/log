eb66e96f6d78f7ba0866ac1bfd1d6f00e277a0a1
Thorjackie <thorjackey@gmail.com>
2022-05-19 01:30:52 +0000

    phone

--- src/assembler.c
+++ src/assembler.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main() {
+int main(int argc, char** argv) {
+	FILE* input;
+	
+	if (argc <= 1) {
+		fprintf(stderr, "no arg supp\n");
+		exit(EXIT_FAILURE);
+	}
+	input = fopen(argv[1], "r");
+	if (input == NULL) {
+		fprintf(stderr, "error opening %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
 	return 0;
 }

4dce4411b677f14e8902ebf89eab70aa354424b0
Thorjackie <thorjackey@gmail.com>
2022-05-20 03:27:42 +0000

    phone edits

--- src/rom.c
+++ src/rom.c
@@ -2,34 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct cmd {
-	int pos;
-	char* name;
-};
-
-struct cmd Sets[] = {
-	{0x00000001, "sACC"},	// Set Accumulator
-	{0x00000002, "sRGS"},	// Set Register Store
-	{0x00000004, "sIR"},	// Set Instruction Register
-	{0x00000008, "sSHF"},	// Set Program Counter Shift
-	{0x00000010, "sPC"},	// Set Program Counter
-	{0x00000020, "sMAR"},	// Set Memory Address Register
-	{0x00000040, "sRAM"},	// Set RAM
-	{0x00000080, "sTMP"},	// Set ALU TMP Register
-	{0x00000100, "sREG"}	// Set Register
-};
-
-struct cmd Enables[] = {
-	{0x00000001, "eACC"},	// Enable Accumulator
-	{0x00000002, "eCT"},	// Enable CT PC Var
-	{0x00000004, "eLD"},	// Enable LD PC Var
-	{0x00000008, "ePC"},	// Enable PC
-	{0x00000010, "eRAM"},	// Enable RAM
-	{0x00000020, "eCRY"},	// Enable Carry In
-	{0x00000040, "eOP0"},	// Enable OP0
-	{0x00000080, "eOP1"},	// Enable OP1
-	{0x00000100, "eREG"}	// Enable Register
-};
+#define FETCH ePC|sMAR, ePCCT|sPC|eRAM|sIRSH, ePC|sMAR, ePCCT|sPC|eRAM|sIRSH, sIR
 
 // Enables
 const int eACC	= 0x0001;
@@ -77,11 +50,12 @@ int main() {
 	};
 	int cmp[] = {
 		eREG
-	int add[];
-	int not[];
-	int and[];
-	int jmp[];
-	int jmpc[];
+	}
+	//int add[];
+	//int not[];
+	//int and[];
+	//int jmp[];
+	//int jmpc[];
 
 	int* data_inst = calloc(16, sizeof(int));
 	if (data_inst == NULL) {

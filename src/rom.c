abe3fdbf5aecb5f20550097b83c7052d0017b3b2
Thorjackie <thorjackey@gmail.com>
2022-05-23 22:07:29 -0500

    laptop

--- src/rom.c
+++ src/rom.c
@@ -5,61 +5,62 @@
 typedef struct INST {
         int data[16];
         size_t size;
+	int len;
 } INST;
 
 // Enables
-const int eACC	= 0x0001; // enable accumulator register
-const int eOP0	= 0x0002; // enable ALU OP 0 bit
-const int eOP1	= 0x0004; // enable ALU OP 1 bit
-const int ePC	= 0x0008; // enable Program Counter
-const int ePCCT	= 0x0010; // enable program counter count bit
-const int ePCLD	= 0x0020; // enable program counter load bit
-const int eRAM	= 0x0040; // enable ram out
-const int eADB	= 0x0080; // enable 16 bit alu mode
-const int eSP	= 0x0100; // enable stack pointer register
-const int eBP	= 0x0200; // enable base pointer register
-const int eRA	= 0x0400; // enable register a
-const int eRB	= 0x0800; // enable register b
+const int eACC	= 0x00010000; // enable accumulator register
+const int eOP0	= 0x00020000; // enable ALU OP 0 bit
+const int eOP1	= 0x00040000; // enable ALU OP 1 bit
+const int ePC	= 0x00080000; // enable Program Counter
+const int ePCCT	= 0x00100000; // enable program counter count bit
+const int ePCLD	= 0x00200000; // enable program counter load bit
+const int eRAM	= 0x00400000; // enable ram out
+const int eADB	= 0x00800000; // enable 16 bit alu mode
+const int eSP	= 0x01000000; // enable stack pointer register
+const int eBP	= 0x02000000; // enable base pointer register
+const int eRA	= 0x04000000; // enable register a
+const int eRB	= 0x08000000; // enable register b
 
 // Sets
-const int sACC	= 0x0001; // set accumulator register
-const int sTMP	= 0x0002; // set alu tmp register
-const int sMAR	= 0x0004; // set memory address register
-const int sPC	= 0x0008; // set program counter
-const int sPCSH	= 0x0010; // set program counter shift register
-const int sSP	= 0x0020; // set stack pointer register
-const int sBP	= 0x0040; // set base pointer register
-const int sRAM	= 0x0080; // set ram
-const int sFLG	= 0x0100; // set flag register
-const int sIRSH = 0x0200; // set instruction shift register
-const int sRST	= 0x0400; // set reset flag
-const int sIR	= 0x0800; // set instruction register
-const int sRA	= 0x1000; // set register a
-const int sRB	= 0x2000; // set register b
+const int sACC	= 0x00000001; // set accumulator register
+const int sTMP	= 0x00000002; // set alu tmp register
+const int sMAR	= 0x00000004; // set memory address register
+const int sPC	= 0x00000008; // set program counter
+const int sPCSH	= 0x00000010; // set program counter shift register
+const int sSP	= 0x00000020; // set stack pointer register
+const int sBP	= 0x00000040; // set base pointer register
+const int sRAM	= 0x00000080; // set ram
+const int sFLG	= 0x00000100; // set flag register
+const int sIRSH = 0x00000200; // set instruction shift register
+const int sRST	= 0x00000400; // set reset flag
+const int sIR	= 0x00000800; // set instruction register
+const int sRA	= 0x00001000; // set register a
+const int sRB	= 0x00002000; // set register b
 
-const INST fetch = {{ePC|sMAR, ePCCT|sPC|eRAM|sIRSH, ePC|sMAR, ePCCT|sPC|eRAM|sIRSH, sIR}, 5};
+const int fetch[] = {ePC|sMAR, ePCCT|sPC|eRAM|sIRSH, ePC|sMAR, ePCCT|sPC|eRAM|sIRSH, sIR};
+const int fetch_size = sizeof fetch;
 
-void writeLine(int* buffer, INST inst, int line) {
-        memcpy(buffer + (16 * line), fetch.data, fetch.size); // add fetch data to buffer
-        memcpy(buffer + (16 * line) + fetch.len, inst.data, inst.data); // add instruction data to buffer
+void writeLine(int* buffer, int* inst, int line, size_t inst_size) {
+	memcpy(buffer + (16 * line), fetch, fetch_size);
+	memcpy(buffer + (16 * line) + (fetch_size / sizeof(int)), inst, inst_size);
 }
 
 int main() {
 
 	int empty[] = {sRST};
-    INST empty = {{sRST}, 1};
 
 	int data[]  = {ePC|sMAR, ePCCT|sPC|eRAM|sRA, sRST};
-	int store[] = {ePC|sMAR, ePCCT|sPC|eRAM|eADB|sTMP, eSP|eADB|sACC, eACC|eADB|sMAR, eRA|sRAM};
-	int load[]  = {ePC|sMAR, ePCCT|sPC|eRAM|eADB|sTMP, eSP|eADB|sACC, eACC|eADB|sMAR, eRAM|sRA};
+	int store[] = {ePC|sMAR, ePCCT|sPC|eRAM|eADB|sTMP, eSP|eADB|sACC, eACC|eADB|sMAR, eRA|sRAM, sRST};
+	int load[]  = {ePC|sMAR, ePCCT|sPC|eRAM|eADB|sTMP, eSP|eADB|sACC, eACC|eADB|sMAR, eRAM|sRA, sRST};
 
-	int add[] = {eRB|sTMP, eRA|sACC, eACC|sRB};
-	int and[] = {eRB|sTMP, eRA|eOP0|sACC, eACC|sRB};
-	int not[] = {eRB|sTMP, eRA|eOP1|sACC, eACC|sRB};
-	int cmp[] = {eRB|sTMP, eRA|eOP0|eOP1|sFLG};
+	int add[] = {eRB|sTMP, eRA|sACC, eACC|sRB, sRST};
+	int and[] = {eRB|sTMP, eRA|eOP0|sACC, eACC|sRB, sRST};
+	int not[] = {eRB|sTMP, eRA|eOP1|sACC, eACC|sRB, sRST};
+	int cmp[] = {eRB|sTMP, eRA|eOP0|eOP1|sFLG, sRST};
 	
-	int jmp[] = {ePC|sMAR, ePCCT|sPC|eRAM|sPCSH, ePC|sMAR, ePCCT|sPC|eRAM|sPCSH, ePCLD|sPC};
-	int jmpc[] = {ePC|sMAR, ePCCT|sPC|eRAM|sPCSH, ePC|sMAR, ePCCT|sPC|eRAM|sPCSH, ePCLD|sPC};
+	int jmp[] = {ePC|sMAR, ePCCT|sPC|eRAM|sPCSH, ePC|sMAR, ePCCT|sPC|eRAM|sPCSH, ePCLD|sPC, sRST};
+	int jmpc[] = {ePC|sMAR, ePCCT|sPC|eRAM|sPCSH, ePC|sMAR, ePCCT|sPC|eRAM|sPCSH, ePCLD|sPC, sRST};
 
 	int* dump = malloc(16*128*sizeof(int));
 
@@ -86,7 +87,16 @@ int main() {
 	writeLine(dump, not, 70, sizeof not);
 	writeLine(dump, cmp, 71, sizeof cmp);
 	writeLine(dump, jmp, 72, sizeof jmp);
-    writeLine(dump, jmpc, 73, sizeof jmpc);
+    	writeLine(dump, jmpc, 73, sizeof jmpc);
+
+	for (int i = 0; i < 128; i++) {
+		if (i % 16 != 0) {
+			printf("%X ", dump[i]);
+		} else {
+			printf("\n%X ", dump[i]);
+		}
+	}
+	printf("\n");
 
 	free(dump);
 	return 0;
